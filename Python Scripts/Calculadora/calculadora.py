eb45d278c6ccdc56e6e30e1b418249d8944e8169
AntonioMallen <aa21.amallen98@iespabloserrano.com>
2022-11-16 20:33:30 +0100

    16/11/2022

--- Python Scripts/Calculadora/calculadora.py
+++ Python Scripts/Calculadora/calculadora.py
@@ -1,167 +1,138 @@
-# importing libraries
-from PyQt6.QtWidgets import *
-from PyQt6 import QtCore, QtGui
-from PyQt6.QtGui import *
-from PyQt6.QtCore import *
+# Import necessary modules
+import sys, json
+from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, 
+	QLineEdit, QCheckBox, QTextEdit, QGridLayout,QPushButton,QGraphicsColorizeEffect, QSizePolicy) 
+from PyQt6.QtCore import Qt, QDate
+from PyQt6.QtGui import QFont
 
-import sys
-
-
-class Window(QMainWindow):
+class MainWindow(QWidget):
 
 	def __init__(self):
+		super().__init__() 
+		self.initializeUI()
+
 		
-		super().__init__()
+		
+
+	def initializeUI(self):
+		"""Set up the application's GUI."""
+		
 		self.setWindowTitle("Calculadora")
-		
-		# setting geometry
+
+
 		self.setGeometry(0, 0, 380, 440)
-
-		# calling method
 		self.UiComponents()
-
-		# showing all the widgets
 		self.show()
 
-		# method for widgets
 	def UiComponents(self):
+		
 		layoutPrincipal = QGridLayout()
-		self.setLayout(layoutPrincipal)
-		layoutNums= QGridLayout()
-		layoutOps=QGridLayout()
-		layoutEq =QGridLayout()
-		layoutResu=QGridLayout()
-		layoutPrincipal.addLayout(layoutNums,0,0)
-		layoutPrincipal.addLayout(layoutOps,0,1)
-		layoutPrincipal.addLayout(layoutResu,1,0)
-		layoutPrincipal.addLayout(layoutEq,1,1)
+		layoutNumeros=QGridLayout()
+		layoutNum1=QGridLayout()
+		layoutNum2=QGridLayout()
+		layoutOp=QGridLayout()
+		layoutRes=QGridLayout()
+		layoutIgual=QGridLayout()
+
+
+		layoutPrincipal.addLayout(layoutNumeros,0,0)
+		layoutPrincipal.addLayout(layoutOp,0,1)
+		layoutPrincipal.addLayout(layoutRes,1,0)
+		layoutPrincipal.addLayout(layoutIgual,1,1)
+ 
+		layoutNumeros.addLayout(layoutNum1,0,0)
+		layoutNumeros.addLayout(layoutNum2,1,0)
+
 
 		# creating a label
-		self.label = QLabel(self)
-
-		# setting geometry to the label
-
+		Resultado=self.label = QLabel(self)
+ 
 		# creating label multi line
 		self.label.setWordWrap(True)
-
+ 
 		# setting style sheet to the label
 		self.label.setStyleSheet("QLabel"
-								"{"
-								"border : 4px solid black;"
-								"background : white;"
-								"}")
-
+								 "{"
+								 "border : 1px solid black;"
+								 "background : white;"
+								 "}")
+ 		
 		# setting alignment to the label
 		self.label.setAlignment(Qt.AlignmentFlag.AlignRight)
 
+ 
 		# setting font
 		self.label.setFont(QFont('Arial', 15))
-
-		
-
-
+		Resultado.setMinimumSize(240,50)
+ 
 		# adding number button to the screen
 		# creating a push button
-
 		push1 = QPushButton("1", self)
-		layoutNums.addWidget(push1,0,0)
-		push1.setFixedSize(58,58)
-		
-		# setting geometry
-		#push1.setGeometry(16, 150, 58, 58)
-		
+		#push1.setFixedSize(58,58)
+		push1.setMinimumSize(58, 58)
+
 		# creating a push button
 		push2 = QPushButton("2", self)
-		layoutNums.addWidget(push2,0,1)
-		push2.setFixedSize(58,58)
-		# setting geometry	 layoutNums.addWidget(push2)
+		push2.setMinimumSize(58,58)
 
-
-
+		
 		# creating a push button
 		push3 = QPushButton("3", self)
-		layoutNums.addWidget(push3,0,2)
-		push3.setFixedSize(58,58)
+		push3.setMinimumSize(58,58)
+
+ 
 		# creating a push button
-
 		push4 = QPushButton("4", self)
-		layoutNums.addWidget(push4,1,0)
-		push4.setFixedSize(58,58)
-		# setting geometry
-
-
+		push4.setMinimumSize(58,58)
 		# creating a push button
 		push5 = QPushButton("5", self)
-
-		# setting geometry
-	
-
+		push5.setMinimumSize(58,58)
 		# creating a push button
 		push6 = QPushButton("6", self)
-
-		# setting geometry
-
-
+		push6.setMinimumSize(58,58)
+ 
 		# creating a push button
 		push7 = QPushButton("7", self)
-
-		# setting geometry
-
+		push7.setMinimumSize(58,58)
+ 
 		# creating a push button
 		push8 = QPushButton("8", self)
-
-		# setting geometry
+		push8.setMinimumSize(58,58)
 
 		# creating a push button
 		push9 = QPushButton("9", self)
-
-		# setting geometry
-
+		push9.setMinimumSize(58,58)
+ 
 		# creating a push button
 		push0 = QPushButton("0", self)
+		push0.setMinimumSize(125,58)
 
-		# setting geometry
-
+ 
 		# adding operator push button
 		# creating push button
 		push_equal = QPushButton("=", self)
-
-		# setting geometry
-
-		## adding equal button a color effect
-		#c_effect = QGraphicsColorizeEffect()
-		#c_effect.setColor(Qt.)
-		#push_equal.setGraphicsEffect(c_effect)
-
+		push_equal.setMinimumSize(87,50)
+ 
+	
 		# creating push button
 		push_plus = QPushButton("+", self)
-
-		# setting geometry
-
+		push_plus.setMinimumSize(58,58)
+ 
 		# creating push button
 		push_minus = QPushButton("-", self)
-
-		# setting geometry
-		
+		push_minus.setMinimumSize(58,58)
+ 
 		# creating push button
 		push_mul = QPushButton("*", self)
-
-		# setting geometry
-	
+		push_mul.setMinimumSize(58,58)
+ 
 		# creating push button
 		push_div = QPushButton("/", self)
-
-		# setting geometry
-	
-
-
-		# clear button
-		push_clear = QPushButton("Supr", self)
-
+		push_div.setMinimumSize(58,58)
 
 		# del one character button
-		push_del = QPushButton("Del", self)
-
+		push_del = QPushButton("Supr", self)
+		push_del.setMinimumSize(58,58)
 
 		# adding action to each of the button
 		push_minus.clicked.connect(self.action_minus)
@@ -179,113 +150,140 @@ class Window(QMainWindow):
 		push_div.clicked.connect(self.action_div)
 		push_mul.clicked.connect(self.action_mul)
 		push_plus.clicked.connect(self.action_plus)
-		push_clear.clicked.connect(self.action_clear)
+
+
 		push_del.clicked.connect(self.action_del)
 
 
-	def action_equal(self):
+		#asigno al layout de numeros los botones de numeros
+		layoutNum1.addWidget(push7,0,0)
+		layoutNum1.addWidget(push8,0,1)
+		layoutNum1.addWidget(push9,0,2)
+		layoutNum1.addWidget(push4,1,0)
+		layoutNum1.addWidget(push5,1,1)
+		layoutNum1.addWidget(push6,1,2)
+		layoutNum1.addWidget(push1,2,0)
+		layoutNum1.addWidget(push2,2,1)
+		layoutNum1.addWidget(push3,2,2)
 
+		layoutNum2.addWidget(push0,0,0)
+		layoutNum2.addWidget(push_del,0,1)
+
+		layoutOp.addWidget(push_plus,0,0)
+		layoutOp.addWidget(push_minus,1,0)
+		layoutOp.addWidget(push_div,2,0)
+		layoutOp.addWidget(push_mul,3,0)
+
+		layoutIgual.addWidget(push_equal,0,0)
+
+		layoutRes.addWidget(Resultado,0,0)
+
+
+		layoutNum1.setHorizontalSpacing(8)
+		layoutNum2.setHorizontalSpacing(7)
+		layoutNum1.setVerticalSpacing(17)
+		layoutNum2.setContentsMargins(0,10,0,0)
+		layoutOp.setVerticalSpacing(17)
+
+
+		self.setLayout(layoutPrincipal)
+	
+	def action_equal(self):
+ 
 		# get the label text
 		equation = self.label.text()
-
+ 
 		try:
 			# getting the ans
 			ans = eval(equation)
-
+ 
 			# setting text to the label
 			self.label.setText(str(ans))
-
+ 
 		except:
 			# setting text to the label
 			self.label.setText("Wrong Input")
-
+ 
 	def action_plus(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + " + ")
-
+ 
 	def action_minus(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + " - ")
-
+ 
 	def action_div(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + " / ")
-
+ 
 	def action_mul(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + " * ")
-
-
+ 
 	def action0(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "0")
-
+ 
 	def action1(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "1")
-
+ 
 	def action2(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "2")
-
+ 
 	def action3(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "3")
-
+ 
 	def action4(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "4")
-
+ 
 	def action5(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "5")
-
+ 
 	def action6(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "6")
-
+ 
 	def action7(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "7")
-
+ 
 	def action8(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "8")
-
+ 
 	def action9(self):
 		# appending label text
 		text = self.label.text()
 		self.label.setText(text + "9")
 
-	def action_clear(self):
-		# clearing the label text
-		self.label.setText("")
-
+ 
 	def action_del(self):
 		# clearing a single digit
 		text = self.label.text()
 		print(text[:len(text)-1])
 		self.label.setText(text[:len(text)-1])
+	
+	
 
-
-# create pyqt5 app
-App = QApplication(sys.argv)
-
-# create the instance of our Window
-window = Window()
-
-# start the app
-sys.exit(App.exec())
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	window = MainWindow()
+	sys.exit(app.exec())
